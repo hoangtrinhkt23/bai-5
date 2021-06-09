dc457d482d142cbfd109e9afc72bb40fd810d3b5
unknown <hoangtrinhkt23@gmail.com>
2021-06-09 21:02:33 +0700

    (thực hành khai báo sử dụng biến

---
+++ index2.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhập vào chiều rộng");
+inputHeight = prompt("nhập vào chiều cao");
+    let width = parseInt(inputWidth)
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("diện tích là: " +area);
+
+}
---
+++ index3.js
@@ -0,0 +1,14 @@
+let inputa;
+let inputb;
+let inputc;
+let inputd;
+inputa=prompt(" nhập vào điểm toán")
+inputb=prompt(" nhập vào điểm văn")
+inputc=prompt(" nhập vào điểm lí")
+inputd=prompt(" nhập vào điểm hóa")
+let a=parseInt(inputa)
+let b=parseInt(inputb)
+let c=parseInt(inputc)
+let d=parseInt(inputd)
+let e= (a+b+c+d)/4
+document.write("điểm trung bình là"+e)
